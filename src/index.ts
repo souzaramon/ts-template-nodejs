2a191816123bf933680a76d68736a9b63c6c15c1
ramonsouza.ema <ramon.souza@emasoftware.com.br>
2022-04-01 23:56:23 -0300

    refactor: update imports to use node protocol

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
-import fs from "fs/promises";
-import path from "path";
-
+import fs from "node:fs/promises";
+import path from "node:path";
 import * as utils from "./index.utils";
 
 async function main() {
